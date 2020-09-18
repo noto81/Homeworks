d0303d88a352bb0cdf1d23965a0f030959ad7a66
noto81 <ing.antoniocalarco@gmail.com>
2020-09-18 08:14:05 -0400

    Homework W3D5 second commit

---
+++ W3D5/map.rb
@@ -0,0 +1,43 @@
+# A map can be implemented using a 2D array. 
+# Let's write a Map class (following a similar pattern to Stack and Queue) that implements a map using only arrays.
+
+# Remember, a map stores information in key-value pairs, where the keys are always unique.
+# When implemented with arrays, a map might look something like this: my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
+
+# Our Map class should have the following instance methods: set(key, value), get(key), delete(key), show. 
+# Note that the set method can be used to either create a new key-value pair or update the value 
+# for a pre-existing key. It's up to you to check whether a key currently exists!
+
+# As always, test your code thoroughly to make sure all properties of maps are enforced.
+
+class Map
+
+    def initialize
+        @map = []
+    end
+
+    #can be used to either create a new key-value pair or update the value 
+    # for a pre-existing key
+    def set(key, value)
+
+    end
+
+    def get(key)
+        @map.each { |pair| return pair[1] if pair[0] == key }
+        nil
+    end 
+    
+    def delete(key)
+    
+    end 
+    
+    def show
+
+    end
+
+end
+
+map = Map.new 
+
+
+
---
+++ W3D5/queue.rb
@@ -0,0 +1,31 @@
+# Now let's write a Queue class. We will need the following instance methods: enqueue(el), dequeue, and peek.
+
+# Test your code to ensure it follows the principle of FIFO.
+
+class Queue
+
+    def initialize
+        @queue = Array.new
+    end
+
+    def enqueue(el)
+        @queue.unshift(el)
+    end
+    
+    def dequeue
+        @queue.pop
+    end 
+    
+    def peek
+        @queue.first
+    end
+
+end
+
+# queue = Queue.new
+# queue.enqueue(1)
+# queue.enqueue(2)
+# queue.enqueue(3)
+# queue.enqueue(4)
+# queue.enqueue(5)
+# queue.peek
