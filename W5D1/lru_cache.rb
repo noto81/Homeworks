169f255837c6fdd0e84cae83f1522d1450bdbd93
noto81 <ing.antoniocalarco@gmail.com>
2020-09-27 15:45:58 -0400

    W5D1 Homework - Basic LRU Cache

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -35,18 +35,48 @@
   end
 
 
-  johnny_cache = LRUCache.new(4)
-  johnny_cache.add("I walk the line")
-  johnny_cache.add(5)
-  johnny_cache.count # => returns 2
-  johnny_cache.add([1,2,3])
-  johnny_cache.add(5)
-  johnny_cache.add(-5)
-  johnny_cache.add({a: 1, b: 2, c: 3})
-  johnny_cache.add([1,2,3,4])
-  johnny_cache.add("I walk the line")
-  johnny_cache.add(:ring_of_fire)
-  johnny_cache.add("I walk the line")
-  johnny_cache.add({a: 1, b: 2, c: 3})
-  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+#   johnny_cache = LRUCache.new(4)
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add(5)
+#   johnny_cache.count # => returns 2
+#   johnny_cache.add([1,2,3])
+#   johnny_cache.add(5)
+#   johnny_cache.add(-5)
+#   johnny_cache.add({a: 1, b: 2, c: 3})
+#   johnny_cache.add([1,2,3,4])
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add(:ring_of_fire)
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add({a: 1, b: 2, c: 3})
+#   johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
 
+# [22] pry(main)> johnny_cache = LRUCache.new(4)      
+# => #<LRUCache:0x00007fffc308bbf8 @cache=[], @size=4>
+# [23] pry(main)> johnny_cache.add("I walk the line")
+# => ["I walk the line"]
+# [24] pry(main)> johnny_cache.add(5)  
+# => ["I walk the line", 5]
+# [25] pry(main)> johnny_cache.count # => returns 2  
+# => 2
+# [26] pry(main)> johnny_cache.add([1,2,3])
+# => ["I walk the line", 5, [1, 2, 3]]
+# [27] pry(main)> johnny_cache.add(5)
+# => ["I walk the line", [1, 2, 3], 5]
+# [28] pry(main)> johnny_cache.add(-5)
+# => ["I walk the line", [1, 2, 3], 5, -5]
+# [29] pry(main)> johnny_cache.add({a: 1, b: 2, c: 3})
+# => [[1, 2, 3], 5, -5, {:a=>1, :b=>2, :c=>3}]
+# [30] pry(main)> johnny_cache.add([1,2,3,4])
+# => [5, -5, {:a=>1, :b=>2, :c=>3}, [1, 2, 3, 4]]
+# [31] pry(main)> johnny_cache.add("I walk the line")
+# => [-5, {:a=>1, :b=>2, :c=>3}, [1, 2, 3, 4], "I walk the line"]
+# [32] pry(main)> johnny_cache.add(:ring_of_fire)
+# => [{:a=>1, :b=>2, :c=>3}, [1, 2, 3, 4], "I walk the line", :ring_of_fire]
+# [33] pry(main)> johnny_cache.add("I walk the line")
+# => [{:a=>1, :b=>2, :c=>3}, [1, 2, 3, 4], :ring_of_fire, "I walk the line"]
+# [34] pry(main)> johnny_cache.add({a: 1, b: 2, c: 3})
+# => [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+# [35] pry(main)>
+# [36] pry(main)> johnny_cache.show
+# [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+# => [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
